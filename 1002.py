f1815319a9abc0a945f31b025a50f5ffe7b68073
Frederico Gomes de Paiva <fredericogdpaiva@gmail.com>
2021-10-13 15:46:09 -0300

    13/10/21

---
+++ 1002.py
@@ -0,0 +1,7 @@
+import math
+
+raio = float(input("Digite o raio do circulo: "))
+
+area = math.pi * math.pow(raio, 2)
+
+print(f"A área do círculo é {area:.4f}")
---
+++ 1004.py
@@ -0,0 +1,6 @@
+valor_1 = int(input('valor 1: '))
+valor_2 = int(input('valor 2: '))
+
+produto = valor_1 * valor_2
+
+print(f'PROD = {produto}')
